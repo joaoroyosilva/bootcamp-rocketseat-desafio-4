28481752fe258a22865e14d13461c3214b9fbdf4
João Gabriel <joao.royosilva@gmail.com>
2019-07-11 19:20:59 -0400

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function header() {
+  return (
+    <header>
+      <nav>
+        <img src="https://i.imgur.com/KDIDiSE.png"/>
+        <div>
+          <span>Meu perfil</span>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  );
+}
